ae2fbb60fce8dfc989075def8223f479f8bdf730
naname-me <whattomson@gmail.com>
2022-11-13 01:47:29 +0300

    New functs

--- Translator_SQL.py
+++ Translator_SQL.py
@@ -1,7 +1,6 @@
-def requests_sql(a,b,c):
+def requests_sql(name_of_table='', funct='' , name_of_colomn='', flag_where=False, post_where='', flag_order_by=False, post_order_by='', asc_desc=True, flag_limit=False, post_limit=''):
 
-    post = a
-
+    post = name_of_table
     aggregate_function = ''
 
     dict_aggregate_functions = {
@@ -12,17 +11,32 @@ def requests_sql(a,b,c):
     }
 
 
-    choice_aggregate_function =  b.lower()
+    choice_aggregate_function =  funct.lower()
 
     for agg_funct in dict_aggregate_functions.keys():
         if choice_aggregate_function in agg_funct:
             aggregate_function = dict_aggregate_functions[agg_funct]
 
-    choice_post = c
+    choice_post = name_of_colomn
 
-    if choice_aggregate_function != 'z':
+    if choice_aggregate_function != '':
         result = f"SELECT {aggregate_function}({choice_post}) FROM {post}"
     else:
         result = f"SELECT {choice_post} FROM {post}"
+
+    if flag_where:
+        where = post_where
+        result += f"WHERE {where}"
+
+    if flag_order_by:
+        order_by = post_order_by
+        result += f"ORDER BY {order_by}"
+        if not(asc_desc):
+            result += 'DESC'
+
+    if flag_limit:
+        limit = post_limit
+        result += f"LIMIT {limit}"
+
 
     return result
