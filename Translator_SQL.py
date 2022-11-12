0726368f41d1ada228a0cd633c542687775dfdae
Administrator <danil36rusdanil@mail.ru>
2022-11-12 18:09:25 +0300

    Beta

--- Translator_SQL.py
+++ Translator_SQL.py
@@ -1,6 +1,7 @@
-def requests_sql():
+from Interfece import *
+def requests_sql(a,b,c):
 
-    post = input("Напишите название таблицы: ")
+    post = a
 
     aggregate_function = ''
 
@@ -12,13 +13,13 @@ def requests_sql():
     }
 
 
-    choice_aggregate_function = input("Выбирите агригантные функцию: ").lower()
+    choice_aggregate_function =  b.lower()
 
     for agg_funct in dict_aggregate_functions.keys():
         if choice_aggregate_function in agg_funct:
             aggregate_function = dict_aggregate_functions[agg_funct]
 
-    choice_post = input("Выберите столбец: ")
+    choice_post = c
 
     if choice_aggregate_function != 'z':
         result = f"SELECT {aggregate_function}({choice_post}) FROM {post}"
--- main.py
+++
@@ -1,8 +0,0 @@
-from Translator_SQL import requests_sql
-
-def main():
-    s = requests_sql()
-    print(s)
-
-if __name__ == '__main__':
-    main()
